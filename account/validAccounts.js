b49a67bd68328a34b6dfc9ed96f56e6ab7ec314f
NGUYỄN THIỆN TÂM <thientam2829@gmail.com>
2023-10-06 10:48:50 +0700

    fix

--- account/validAccounts.js
+++ account/validAccounts.js
@@ -1,9 +1,9 @@
 // validAccounts.js
 
 const validAccounts = [
-    {username: "abc", password: "123"},
-    {username: "user2", password: "password2"},
-    // Thêm tài khoản và mật khẩu khác nếu cần
+  { username: "abc", password: "123" },
+  { username: "user2", password: "password2" },
+  // Thêm tài khoản và mật khẩu khác nếu cần
 ];
 
 export default validAccounts;
